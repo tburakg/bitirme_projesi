53f7ada4474ddb4d8c2485e527179eb3133dbd2a
Talha Burak Gürsel <tburakgursel@gmail.com>
2023-04-30 14:08:36 +0300

    convert to turkish

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def kararVer():
     with sr.Microphone(device_index=3) as source:
         print("başka işlem yapmak istiyor musunuz 5 saniyeniz var")
         audio_data = r.record(source, duration=5)
-        print("Recognizing...")
+        print("Tanımlanıyor...")
         karar = r.recognize_google(audio_data)
         print(karar)
     return karar
@@ -27,19 +27,18 @@ def notAl():
     with sr.Microphone(device_index=3) as source:
         print("Neyi not almak istersin")
         audio_data = r.record(source, duration=5)
-        print("Recognizing...")
+        print("Tanımlanıyor...")
         notKarar = r.recognize_google(audio_data)
         print("Yazdırılıyor: ", notKarar)
     return notKarar
 
-print("asdadas","\n","nasddsa")
 while True:
     #sr.Microphone.list_microphone_names()  mikrofonun kaçıncı indexteyse onu yaz
     with sr.Microphone(device_index=3) as source:
         print("You Can Speak")
         # read the audio data from the default microphone
         audio_data = r.record(source, duration=5)
-        print("Recognizing...")
+        print("Tanımlanıyor...")
         text = r.recognize_google(audio_data, language="tr")
         print(text)
     query = text.lower()
@@ -48,9 +47,9 @@ while True:
         # http eklemezen internet explorer da açıyor
         webbrowser.open("http://youtube.com")
         karar = kararVer()
-        if "no" in karar:
+        if "hayır" in karar:
             break
-        elif "yes" in karar:
+        elif "evet" in karar:
             continue
 
     elif 'pdf aç' in query:
@@ -58,27 +57,27 @@ while True:
         os.startfile(filePath)
 
         karar = kararVer()
-        if "no" in karar:
+        if "hayır" in karar:
             break
-        elif "yes" in karar:
+        elif "evet" in karar:
             continue
 
     elif 'video aç' in query:
         filePath = "video1.mp4"
         os.startfile(filePath)
         karar = kararVer()
-        if "no" in karar:
+        if "hayır" in karar:
             break
-        elif "yes" in karar:
+        elif "evet" in karar:
             continue
 
     elif 'fotoğraf aç' in query:
         filePath = "image1.jpg"
         os.startfile(filePath)
         karar = kararVer()
-        if "no" in karar:
+        if "hayır" in karar:
             break
-        elif "yes" in karar:
+        elif "evet" in karar:
             continue
 
     elif 'not al' in query:
@@ -89,18 +88,18 @@ while True:
             f.write(not_karar + "\n")
 
         karar = kararVer()
-        if "no" in karar:
+        if "hayır" in karar:
             break
-        elif "yes" in karar:
+        elif "evet" in karar:
             continue
 
     elif 'şarkı aç' in query:
         filePath = "sample.mp3"
         os.startfile(filePath)
         karar = kararVer()
-        if "no" in karar:
+        if "hayır" in karar:
             break
-        elif "yes" in karar:
+        elif "evet" in karar:
             continue
 
 
